05fbeb8651cc3a58633f64ae2363d9ba1d8ddeac
auttapon37 <istudents@iClassrooms-iMac-20.local>
2015-01-16 15:49:42 +0700

    testSwift

---
+++ SwiftTutorial_1.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
