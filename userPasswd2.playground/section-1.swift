74ddd55dbc72f2fa4e2038d19caaefdbf2f8c1b6
auttapon37 <istudents@iClassrooms-iMac-17.local>
2015-01-23 17:16:57 +0700

    passwd

--- userPasswd2.playground/section-1.swift
+++ userPasswd2.playground/section-1.swift
@@ -2,4 +2,64 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class idpass {
+    let userpassword: Dictionary<String, String> = ["Bao":"2032","Bear":"2155","Jane":"2042", "Bom":"2025","Nan":"2043"]
+
+    
+    func check(name:String) {
+        if(userpassword.isEmpty){
+            println("Empty")
+        }else{
+            println("Not empty")
+        }
+    }
+    
+    //////////////
+    let userInput:String
+    let inputPass:String
+    
+    init(user:String , pass:String){
+        self.userInput=user
+        self.inputPass=pass
+        self.userchang=user
+        self.Passchang=pass
+    }
+    
+    func add(user:String,pass:String){
+        userpassword[user]=pass
+    }
+    
+    let userchang:String
+    let Passchang:String
+    
+    
+    func update(Passchang:String,userchang:String) {
+        
+        if let old = userpassword.updateValue(Passchang, forKey: userpassword){
+        //if let oldvalue = userpassword.updateValue(Passchang, forKey:userchang){
+            
+            println("The old value for data was \(oldvalue).")
+            
+        }
+    }
+    
+    func delet(userDel:String) {
+      
+        userpassword[userDel]=nil
+    }
+    func ser(user:String){
+        for(key,value) in userpassword{
+            if(key==user){
+                println("Yes\(value)")
+            }
+        }
+        
+    }
+    
+    
+}
+
+let start = idpass(user: "Bao",pass: "2032")
+//start.add("tttt",pass :"123456")
+start.update("21555", userchang: "Bear")
+start.ser("Bao")
