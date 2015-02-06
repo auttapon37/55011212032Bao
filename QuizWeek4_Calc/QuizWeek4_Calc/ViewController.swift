c4d4b57913a965e6df5de907c531a19d87df28ae
auttapon37 <istudents@iClassrooms-iMac-17.local>
2015-02-06 17:13:25 +0700

    quiz4

--- QuizWeek4_Calc/QuizWeek4_Calc/ViewController.swift
+++ QuizWeek4_Calc/QuizWeek4_Calc/ViewController.swift
@@ -20,6 +20,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var sum1=0,sum2=0,sum3=0
+    @IBAction func oneBT(sender: AnyObject) {
+        
+        sum1 = sum1 + 1
+        oneLabel.text = String(sum1)
+        
+    }
 
+    @IBAction func twoBT(sender: AnyObject) {
+        sum2 = sum2 + 2
+        twoLabel.text = String(sum2)
+    }
+    
+    @IBAction func threeBT(sender: AnyObject) {
+        sum3 = sum3 + 3
+        threeLabel.text = String(sum3)
+    }
+    
+    @IBAction func resetBT(sender: AnyObject) {
+        oneLabel.text = "0"
+        twoLabel.text = "0"
+        threeLabel.text = "0"
+        sum1=0
+        sum2=0
+        sum3=0
+    }
+    
+    @IBOutlet weak var oneLabel: UILabel!
+    @IBOutlet weak var twoLabel: UILabel!
+    @IBOutlet weak var threeLabel: UILabel!
 }
 
